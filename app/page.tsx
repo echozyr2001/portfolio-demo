b3a2e1ea6161bee21ac1bb7f332e1a61b818d0d1
echozyr2001 <echo.zyr.2001@gmail.com>
2025-03-14 21:53:37 +0800

    Projects mobile style

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/sections/Header";
 import { Hero } from "@/components/sections/Hero";
+import { Projects } from "@/components/sections/Projects";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Projects />
     </div>
   );
 }

d572e014e5f517425b519a35e88a913d2d16bfdc
echozyr2001 <echo.zyr.2001@gmail.com>
2025-03-16 19:21:47 +0800

    Projects sticky

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -56,10 +56,13 @@ export function Projects() {
         />
 
         <div className="flex flex-col mt-10 gap-20 md:mt-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, i) => (
             <Card
               key={project.title}
-              className="px-8 pt-8 pb-0 md:px-10 md:pt-12 lg:pt-16 lg:px-20"
+              className="px-8 pt-8 pb-0 md:px-10 md:pt-12 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${i * 40}px)`,
+              }}
             >
               <div className="lg:grid-cols-2 lg:grid lg:gap-16">
                 <div className="lg:pb-16">

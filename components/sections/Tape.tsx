4f64e719d873afe7ddd1d7450ea16463c29a3bea
echozyr2001 <echo.zyr.2001@gmail.com>
2025-03-17 21:11:08 +0800

    Tape animate

--- components/sections/Tape.tsx
+++ components/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import { Sparkle } from "lucide-react";
+import { Fragment } from "react";
 
 const words = [
   "Performant",
@@ -20,17 +21,21 @@ export function Tape() {
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1">
         {/* 这里 flex 与 flex-none 的使用没有搞明白 */}
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3 ">
-            {words.map((word) => {
-              return (
-                <div key={word} className="inline-flex gap-4 items-center">
-                  <span className="text-gray-900 uppercase font-extrabold text-sm">
-                    {word}
-                  </span>
-                  <Sparkle className="size-6 text-gray-900 -rotate-20" />
-                </div>
-              );
-            })}
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]">
+            {[...new Array(2)].fill(0).map((_, i) => (
+              <Fragment key={i}>
+                {words.map((word) => {
+                  return (
+                    <div key={word} className="inline-flex gap-4 items-center">
+                      <span className="text-gray-900 uppercase font-extrabold text-sm">
+                        {word}
+                      </span>
+                      <Sparkle className="size-6 text-gray-900 -rotate-20" />
+                    </div>
+                  );
+                })}
+              </Fragment>
+            ))}
           </div>
         </div>
       </div>

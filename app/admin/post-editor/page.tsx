3d9d31d14d94567f36e93730f80d12c8bdc01512
echozyr2001 <echo.zyr.2001@gmail.com>
2025-08-21 20:57:00 +0800

    delete example

--- app/admin/post-editor/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PostEditorExample } from "@/components/admin/PostEditor";
-
-export default function PostEditorPage() {
-	return (
-		<div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-			<PostEditorExample />
-		</div>
-	);
-}

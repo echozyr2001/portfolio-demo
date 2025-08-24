f9dd3215f84e54b203e5d18b4a3eb8eb388258c7
echozyr2001 <echo.zyr.2001@gmail.com>
2025-08-24 15:19:51 +0800

    delete useless

--- components/admin/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as AdminLayout } from "./AdminLayout";
-export { default as ContentList } from "./ContentList";
-export { default as CategoryManager } from "./CategoryManager";
-export { default as TagManager } from "./TagManager";
-export { default as MediaLibrary } from "./MediaLibrary";
-export { default as StatusBadge } from "./StatusBadge";
